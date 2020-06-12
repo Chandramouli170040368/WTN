e00aca5315be75ce650e7798ac6be42b856cfb79
moulidatta <moulidatta@DESKTOP-I1CDTBR>
2020-06-12 10:00:04 +0530

    Command Line Arguments

---
+++ languagebasics/src/languagebasics/ex1.java
@@ -0,0 +1,10 @@
+package languagebasics;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		System.out.println(args[0]+" Technologies "+args[1]);
+
+	}
+
+}
---
+++ languagebasics/src/languagebasics/ex3.java
@@ -0,0 +1,9 @@
+package languagebasics;
+
+public class ex3 {
+
+	public static void main(String args[]) {
+		System.out.println("Welcome "+args[0]);
+	}
+
+}
