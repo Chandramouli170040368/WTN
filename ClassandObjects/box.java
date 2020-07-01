0a2218c493ea50ed05e83ea8503fa8b4f5132eb0
Chandramouli170040368 <66763693+Chandramouli170040368@users.noreply.github.com>
2020-07-01 11:18:43 +0530

    Add files via upload

--- ClassandObjects/box.java
+++ ClassandObjects/box.java
@@ -1,1 +1,23 @@
+package classes_objects;
 
+public class box {
+	 double h,w,d;
+
+     box(double width,double height,double depth)
+    {
+        h=height;
+        w=width;
+        d=depth;
+    }
+    double volume()
+    { double v;
+    v=h*w*d;
+    return v;
+    }
+   
+   	public static void main(String[] args) {
+	 box bc = new box(6.5,8.3,3.5);
+	    System.out.println(bc.volume());
+
+}
+}
